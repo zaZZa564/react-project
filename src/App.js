97e5463f407d3b97f8be08046339a13b418e87f0
zaZZa564 <anus_bobra564@mail.ru>
2025-03-02 20:14:26 +0300

    header & styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 class App extends React.Component {
   render() {
     return (
-      <div>placeholder</div>
+      <div>
+        <Header title='user list' />
+      </div>
     );
   }
 }
