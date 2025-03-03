dfb31c6661facb0138f16479eeb45f8d20788347
zaZZa564 <anus_bobra564@mail.ru>
2025-03-03 22:31:21 +0300

    iocns & css

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { IoCloseCircleSharp, IoHammerSharp } from "react-icons/io5";
 
 class Car extends React.Component {
   car = this.props.car;
   render() {
     return (
         <div className="car">
+          <IoCloseCircleSharp className="delete-icon" />
+          <IoHammerSharp className="edit-icon" />
           <h3>{this.car.model}</h3>
           <p>brend: {this.car.brend}</p>
           <p>tier:{this.car.tier}</p>
