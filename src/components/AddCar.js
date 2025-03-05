73480d69f9e8b126b080154169ed4d5362ec31d5
zaZZa564 <anus_bobra564@mail.ru>
2025-03-05 22:11:49 +0300

    AddCar states

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -1,15 +1,39 @@
 import React from "react";
 
 class AddCar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brend: '',
+      tier: '',
+      model: '',
+      horsePower: '',
+      seats: '',
+      price: ''
+    }
+  }
+
   render() {
     return (
       <form>
-        <input placeholder="brend"/>
-        <input placeholder="tier"/>
-        <input placeholder="model"/>
-        <input placeholder="horsePower"/>
-        <input placeholder="seats"/>
-        <input placeholder="price"/>
+        <input placeholder="brend"
+          onChange={(e) => {this.setState({brend: e.target.value})}}
+        />
+        <input placeholder="tier"
+          onChange={(e) => {this.setState({tier: e.target.value})}}
+        />
+        <input placeholder="model"
+          onChange={(e) => {this.setState({model: e.target.value})}}
+        />
+        <input placeholder="horsePower"
+          onChange={(e) => {this.setState({horsePower: e.target.value})}}
+        />
+        <input placeholder="seats"
+          onChange={(e) => {this.setState({seats: e.target.value})}}
+        />
+        <input placeholder="price"
+          onChange={(e) => {this.setState({price: e.target.value})}}
+        />
         <button type="button">add</button>
       </form>
     );
