f7a196bd683e7e497df031f8a4d464da6fb248f3
zaZZa564 <anus_bobra564@mail.ru>
2025-03-06 10:20:34 +0300

    form logic

--- src/App.js
+++ src/App.js
@@ -4,12 +4,56 @@ import Cars from "./components/Cars";
 import AddCar from "./components/AddCar";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars : [
+        {
+          id: 1,
+          brend: 'Archer',
+          tier: 'middle',
+          model: 'Archer Hella EC-D i360',
+          horsePower: 212,
+          seats: 4,
+          price: 1500
+        },
+        {
+          id: 2,
+          brend: 'Delamain',
+          tier: 'corporate',
+          model: 'Delamain Cab',
+          horsePower: 333,
+          seats: 4,
+          price: 2000
+        },
+        {
+          id: 3,
+          brend: 'Herreca',
+          tier: 'upper',
+          model: 'Herrera Outlaw GTS',
+          horsePower: 755,
+          seats: 4,
+          price: 7500
+        },
+        {
+          id: 4,
+          brend: 'Rayfield',
+          tier: 'upper',
+          model: 'Rayfield Caliburn',
+          horsePower: 1660,
+          seats: 2,
+          price: 2400
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
         <Header title='car list' />
         <main>
-          <Cars />
+          <Cars carsList={this.state.cars}/>
         </main>
         <aside>
           <AddCar />
--- src/components/Cars.js
+++ src/components/Cars.js
@@ -2,55 +2,11 @@ import React from "react";
 import Car from "./Car";
 
 class Cars extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars : [
-        {
-          id: 1,
-          brend: 'Archer',
-          tier: 'middle',
-          model: 'Archer Hella EC-D i360',
-          horsePower: 212,
-          seats: 4,
-          price: 1500
-        },
-        {
-          id: 2,
-          brend: 'Delamain',
-          tier: 'corporate',
-          model: 'Delamain Cab',
-          horsePower: 333,
-          seats: 4,
-          price: 2000
-        },
-        {
-          id: 3,
-          brend: 'Herreca',
-          tier: 'upper',
-          model: 'Herrera Outlaw GTS',
-          horsePower: 755,
-          seats: 4,
-          price: 7500
-        },
-        {
-          id: 4,
-          brend: 'Rayfield',
-          tier: 'upper',
-          model: 'Rayfield Caliburn',
-          horsePower: 1660,
-          seats: 2,
-          price: 2400
-        }
-      ]
-    }
-  }
-
   render() {
-    if(this.state.cars.length > 0) {
+    if(this.props.carsList.length > 0) {
       return (
         <div>
-          {this.state.cars.map((el) => (
+          {this.props.carsList.map((el) => (
             <Car key={el.id} car={el}/>
           ))}
         </div>
